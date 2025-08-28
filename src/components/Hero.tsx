ff550591d33c323a087e7f298cc9b4a25bc4ca36
Prajwal Singh <102660402+Singh-Prajwal@users.noreply.github.com>
2025-08-29 03:14:27 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -39,6 +39,7 @@ export default function Hero() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.4 }}
+        id={"visitbtn"}
       >
         View My Work <ArrowDown size={20} />
       </motion.a>
